f944286977042507a0bc35218fd978125b9e431d
Adhikari Chandra Vamsi <chandravamsi.105@gmail.com>
2026-01-04 01:13:18 +0530

    Add root route

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,9 @@ const cors = require("cors")
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Backend is running");
+});
 
 app.get("/api/profile", (req, res) => {
   console.log("API HIT")
@@ -16,6 +19,7 @@ app.get("/api/profile", (req, res) => {
 })
 
 const PORT = process.env.PORT || 5050
+
 app.listen(PORT, () => {
-  console.log(`Backend running on http://localhost:${PORT}`)
+  console.log(`Backend running on port ${PORT}`)
 })
